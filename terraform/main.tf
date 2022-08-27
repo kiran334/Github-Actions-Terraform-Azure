649b081ab14f4a0bf5f2d50981fbdcab16868248
kiran334 <112266740+kiran334@users.noreply.github.com>
2022-08-27 13:03:53 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-rg"
-    storage_account_name = "myterraformsadev1"
+    storage_account_name = "myterraformsadev"
     container_name       = "tfstatefile"
     key                  = "dev.terraform.tfstate"
   }
