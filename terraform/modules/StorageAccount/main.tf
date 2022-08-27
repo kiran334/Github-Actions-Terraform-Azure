6339f06619e1a579b494286f1a08c7e2c6b3b0e5
kiran334 <112266740+kiran334@users.noreply.github.com>
2022-08-27 12:36:50 -0400

    Update main.tf

--- terraform/modules/StorageAccount/main.tf
+++ terraform/modules/StorageAccount/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_storage_account" "STA" {
   resource_group_name      = var.rgname
   location                 = var.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
 
   tags = {
     environment = "dev"
