782f6a56abe69994fa55fc53d9175cbb198e7386
kiran334 <112266740+kiran334@users.noreply.github.com>
2022-08-27 12:51:19 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "rgname" {
   description = "Resource Group Name"
-  default     = "github-action1"
+  default     = "devtfrg1"
   type        = string
 }
 variable "location" {
   description = "Azure location"
-  default     = "East US"
+  default     = "East US2"
   type        = string
 }
 variable "sname" {
